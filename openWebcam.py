27855265eef7b4681537406c68298ca8f85f35fb
Yash <myyash.ask@gmail.com>
2026-06-16 17:00:15 +0100

    feat: hand landmark visual dot tracking in real time

--- openWebcam.py
+++ openWebcam.py
@@ -20,20 +20,30 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 # Create a hand landmarker instance with the live stream mode:
 def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     print('hand landmarker result: {}'.format(result))
+    global latest_result
+    latest_result = result
 
 options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path='model_path/hand_landmarker.task'),
     running_mode=VisionRunningMode.LIVE_STREAM,
     result_callback=print_result)
 
-
 with HandLandmarker.create_from_options(options) as landmarker:
+    latest_result = None
+
     while True:
         timestamp = int(time.time() * 1000)
         ret, frame = cam.read()
 
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
         landmarker.detect_async(mp_image, timestamp)
+
+        if latest_result:
+            for hand_landmarks in latest_result.hand_landmarks:
+                for landmark in hand_landmarks:
+                    x = int(landmark.x * frame.shape[1])
+                    y = int(landmark.y * frame.shape[0])
+                    cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
     
         # Display the captured frame
         cv2.imshow('Camera', frame)
